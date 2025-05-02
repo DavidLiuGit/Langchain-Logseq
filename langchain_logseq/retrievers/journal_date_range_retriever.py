ef1f5526a59a02e22d69d3e91240af3c68712092
DL <dvdgege5@gmail.com>
2025-05-02 12:39:47 -0500

    Update Retriever impl, by adding Contextualizer

--- langchain_logseq/retrievers/journal_date_range_retriever.py
+++ langchain_logseq/retrievers/journal_date_range_retriever.py
@@ -1,9 +1,18 @@
-from typing import Any, Optional
+from logging import getLogger
+from typing import TYPE_CHECKING
 
 from langchain_core.documents import Document
-from langchain_core.runnables import RunnableConfig
+
 from langchain_logseq.loaders import LogseqJournalLoader
 from langchain_logseq.retrievers import LogseqJournalRetriever
+from langchain_logseq.retrievers.contextualizer import RetrieverContextualizer
+from langchain_logseq.loaders.logseq_journal_loader_input import LogseqJournalLoaderInput
+
+if TYPE_CHECKING:
+    from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
+
+
+logger = getLogger(__name__)
 
 
 class LogseqJournalDateRangeRetriever(LogseqJournalRetriever):
@@ -13,12 +22,26 @@ class LogseqJournalDateRangeRetriever(LogseqJournalRetriever):
     
     def __init__(
         self,
+        contextualizer: RetrieverContextualizer,
         loader: LogseqJournalLoader,
     ):
+        if not isinstance(contextualizer, RetrieverContextualizer):
+            raise TypeError("Contextualizer must be an instance of RetrieverContextualizer")
+        self.contextualizer = contextualizer
+
         if not isinstance(loader, LogseqJournalLoader):
             raise TypeError("Loader must be an instance of LogseqJournalLoader")
         self.loader = loader
 
 
-    def invoke(self, input: str, config: Optional[RunnableConfig] = None, **kwargs: Any) -> list[Document]:
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> list[Document]:
+        # determine the relevant 
 
+
+    # def build_loader_input(self, query: str) -> LogseqJournalLoaderInput:
+        """
+        Based on the natural-language `query`, return an instance of `LogseqJournalLoaderInput`,
+        which can then be used to invoke the `LogseqJournalLoader`.
+        """
