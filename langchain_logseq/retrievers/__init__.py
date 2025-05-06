a0fd98cad8d9ff27064af20cac45e0c4f89d844f
DL <dvdgege5@gmail.com>
2025-05-05 22:56:44 -0500

    Add LogseqJournalRetriever.retrieve() method
    Lays the foundation for potentially moving away from Langchain's
    convoluted Retriever.invoke()

--- langchain_logseq/retrievers/__init__.py
+++ langchain_logseq/retrievers/__init__.py
@@ -7,14 +7,19 @@ from langchain_core.retrievers import BaseRetriever
 from langchain_core.messages import BaseMessage
 
 
-
 class LogseqJournalRetriever(BaseRetriever):
     """
     A Langchain `Retriever` that is specifically for retrieving Logseq journal `Document`'s,
     based on a natural-language query. This `Retriever` will, in turn, leverage a Loader or
     Vectorstore to retrieve relevant documents to the query.
     """
-    
+
+    def retrieve(self, query: str, chat_history: Optional[list[BaseMessage]] = None) -> list[Document]:
+        """
+        Called by `invoke` to retrieve relevant documents to the query.
+        """
+        return self._get_relevant_documents(query, chat_history=chat_history)
+
     @abstractmethod
     def _get_relevant_documents(
         self,
@@ -25,7 +30,7 @@ class LogseqJournalRetriever(BaseRetriever):
     ) -> list[Document]:
         """
         Called by `invoke`.
-        
+
         `Retriever`s accept `input`, a natural-language "query", and returns a list of potentially
         relevant `Document`s to answer the query.
         This specific `Retriever` loads Logseq journal snippets from a date range, which will be determined
