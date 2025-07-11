0750981b02b8d66e1db2386959862c34da2c254e
DL <dvdgege5@gmail.com>
2025-07-10 23:26:13 -0500

    Integrate pgvector-template, impl abstract classes
    Squashed commit of the following:
    
    commit 70b9b45e7ed22798f1b233d8e591d807b20d21a2
    Author: DL <dvdgege5@gmail.com>
    Date:   Thu Jul 10 01:04:08 2025 -0500
    
        Impl extract_anchor_ids; unit test
    
    commit c3ed860c18e2e6f6b9e3c23718a92514226adc48
    Author: DL <dvdgege5@gmail.com>
    Date:   Thu Jul 10 00:42:23 2025 -0500
    
        Update logic for extract_chunk_references; more unit test coverage
    
    commit ece110a443b7b05986cf0602c046afe4c2d1c8b4
    Author: DL <dvdgege5@gmail.com>
    Date:   Thu Jul 10 00:17:17 2025 -0500
    
        Impl _extract_chunk_references; update _split_corpus w/ removeprefix
    
    commit 8019659f8987165e3e9f2f56496482d613ea0aca
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jul 9 11:35:27 2025 -0500
    
        Impl basic integ test for PGVector JournalCorpusManager
    
    commit cd6dd28f7b97c3434e536091b6fac30d676ac3b9
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jul 9 11:35:08 2025 -0500
    
        Strip each chunk before returning from _split_corpus
    
    commit cc8ff4dc2a10bb8f2d50a091057c732c01b8e7af
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jul 9 11:18:18 2025 -0500
    
        Impl unit tests for JournalCorpusManager
    
    commit 9410c76e7e2eef84e54321213499d0625147a4cc
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jul 9 11:04:27 2025 -0500
    
        Move JournalCorpusManager to pgvector module
    
    commit 78ca499bfb0f272066a5372454ac099b5dd7619e
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jun 18 12:42:47 2025 -0500
    
        Impl JournalCorpusManagerConfig
    
    commit f6b4027873c4b6643878246c71f28c5ff507d925
    Author: DL <dvdgege5@gmail.com>
    Date:   Wed Jun 18 12:23:08 2025 -0500
    
        Update journal document template
    
    commit c5dbe723b4600e546c0c6a0b621477b0cecd086b
    Author: DL <dvdgege5@gmail.com>
    Date:   Tue Jun 17 12:10:39 2025 -0500
    
        Skeleton impl for pgvector-template integ
    
    commit 057389d4a586f10ace2e687da781a20b2dab7d1e
    Author: DL <dvdgege5@gmail.com>
    Date:   Mon Jun 16 23:54:13 2025 -0500
    
        Add pgvector-template dep

---
+++ tests/uploaders/pgvector/test_journal_corpus_manager.py
@@ -0,0 +1,204 @@
+import unittest
+from unittest.mock import Mock
+from pgvector_template.core.embedder import BaseEmbeddingProvider
+from langchain_logseq.uploaders.pgvector.journal_corpus_manager import JournalCorpusManager, JournalCorpusManagerConfig
+
+
+class TestJournalCorpusManager(unittest.TestCase):
+
+    def setUp(self):
+        mock_session = Mock()
+        mock_embedding_provider = Mock(spec=BaseEmbeddingProvider)
+        config = JournalCorpusManagerConfig(embedding_provider=mock_embedding_provider)
+        self.corpus_manager = JournalCorpusManager(mock_session, config)
+
+    def test_split_corpus_basic(self):
+        content = "First line\n- First bullet\n- Second bullet"
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, ["First line", "First bullet", "Second bullet"])
+
+    def test_split_corpus_empty_chunks_filtered(self):
+        content = "First line\n-\n- Valid bullet\n-   "
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, ["First line", "Valid bullet"])
+
+    def test_split_corpus_no_bullets(self):
+        content = "Just some text without bullets"
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, ["Just some text without bullets"])
+
+    def test_extract_chunk_metadata(self):
+        content = "This is a test chunk with five words"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 36,
+                "word_count": 8,
+                "references": [],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_extract_chunk_metadata_empty(self):
+        content = ""
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 0,
+                "word_count": 0,
+                "references": [],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_split_corpus_only_bullets(self):
+        content = "- First bullet\n- Second bullet"
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, ["First bullet", "Second bullet"])
+
+    def test_split_corpus_empty_string(self):
+        content = ""
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, [])
+
+    def test_split_corpus_whitespace_only(self):
+        content = "   \n\t  "
+        result = self.corpus_manager._split_corpus(content)
+        self.assertEqual(result, [])
+
+    def test_extract_chunk_references_basic(self):
+        split_content = ["this", "is", "#my", "test", "#script"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["my", "script"])
+
+    def test_extract_chunk_references_complex(self):
+        split_content = ["this", "is", "#my", "test", "#script'sfatal", "flaw", "#lol#"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["my", "script", "lol"])
+
+    def test_extract_chunk_references_none(self):
+        split_content = ["no", "references", "here"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, [])
+
+    def test_extract_chunk_references_empty(self):
+        split_content = []
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, [])
+
+    def test_extract_chunk_references_dates(self):
+        split_content = ["met", "with", "#2025-07-07", "about", "#cookout"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["2025-07-07", "cookout"])
+
+    def test_extract_chunk_metadata_with_references(self):
+        content = "Met with team about #project-alpha and #2025-01-15"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 50,
+                "word_count": 7,
+                "references": ["project-alpha", "2025-01-15"],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_extract_chunk_metadata_special_chars(self):
+        content = "Check #bug-fix! and #feature@v2 (important)"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 43,
+                "word_count": 5,
+                "references": ["bug-fix", "feature@v2"],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_extract_chunk_metadata_multiple_spaces(self):
+        content = "Review   #docs    with   #team-lead   tomorrow"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 46,
+                "word_count": 5,
+                "references": ["docs", "team-lead"],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_extract_chunk_metadata_newlines_tabs(self):
+        content = "Task\t#urgent\nfollow up #meeting-notes"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 37,  # note: special chars like \t & \n only count as 1 char each
+                "word_count": 5,
+                "references": ["urgent", "meeting-notes"],
+                "anchor_ids": [],
+            },
+        )
+
+    def test_extract_chunk_references_breaking_chars(self):
+        split_content = ["#asdf?qwer", "#test!end", "#name:value", "#quote'break", '#double"quote']
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["asdf", "test", "name", "quote", "double"])
+
+    def test_extract_chunk_references_backslash_ignored(self):
+        split_content = ["#asdf\\qwer", "#test\\\\path", "#name\\value"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["asdfqwer", "testpath", "namevalue"])
+
+    def test_extract_chunk_references_mixed_special_chars(self):
+        split_content = ["#tag\\with?break", "#path\\to:file", "#name'with\"quotes"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["tagwith", "pathto", "name"])
+
+    def test_extract_chunk_references_multiple_hashes(self):
+        split_content = ["###ref", "##tag", "####multiple"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["ref", "tag", "multiple"])
+
+    def test_extract_chunk_references_trailing_hashes(self):
+        split_content = ["#lol#", "#tag##", "#ref###"]
+        result = self.corpus_manager._extract_chunk_references(split_content)
+        self.assertEqual(result, ["lol", "tag", "ref"])
+
+    def test_extract_anchor_ids_single(self):
+        content = "Some text\n  id:: 686f4ac0-e43b-4a15-940a-954f55e03bea\nMore text"
+        result = self.corpus_manager._extract_anchor_ids(content)
+        self.assertEqual(result, ["686f4ac0-e43b-4a15-940a-954f55e03bea"])
+
+    def test_extract_anchor_ids_multiple(self):
+        content = "First id:: 686f4ac0-e43b-4a15-940a-954f55e03bea\nSecond id:: 12345678-1234-1234-1234-123456789abc"
+        result = self.corpus_manager._extract_anchor_ids(content)
+        self.assertEqual(result, ["686f4ac0-e43b-4a15-940a-954f55e03bea", "12345678-1234-1234-1234-123456789abc"])
+
+    def test_extract_anchor_ids_none(self):
+        content = "No anchor IDs here, just regular text"
+        result = self.corpus_manager._extract_anchor_ids(content)
+        self.assertEqual(result, [])
+
+    def test_extract_anchor_ids_with_references(self):
+        content = "Text with ((686f4ac0-e43b-4a15-940a-954f55e03bea)) reference but id:: 12345678-1234-1234-1234-123456789abc anchor"
+        result = self.corpus_manager._extract_anchor_ids(content)
+        self.assertEqual(result, ["12345678-1234-1234-1234-123456789abc"])
+
+    def test_extract_chunk_metadata_with_anchor_ids(self):
+        content = "Task with #tag\n  id:: 686f4ac0-e43b-4a15-940a-954f55e03bea"
+        result = self.corpus_manager._extract_chunk_metadata(content)
+        self.assertEqual(
+            result,
+            {
+                "chunk_len": 58,
+                "word_count": 5,
+                "references": ["tag"],
+                "anchor_ids": ["686f4ac0-e43b-4a15-940a-954f55e03bea"],
+            },
+        )
