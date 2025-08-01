ced23da9c9bb7d639cd0d32f56e43691311961f5
DL <dvdgege5@gmail.com>
2025-08-01 00:58:11 -0500

    Export retrievers; bump pgvector-template version

--- langchain_logseq/retrievers/__init__.py
+++ langchain_logseq/retrievers/__init__.py
@@ -2,3 +2,4 @@
 from langchain_logseq.retrievers.contextualizer import *
 from langchain_logseq.retrievers.journal_retriever import *
 from langchain_logseq.retrievers.journal_date_range_retriever import *
+from langchain_logseq.retrievers.pgvector_journal_retriever import *
